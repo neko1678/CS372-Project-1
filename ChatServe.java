afd16d08eab22c15e766d7a6f692354ca8aec4a0
neko1678 <neal.kornreich@gmail.com>
2019-11-03 16:32:45 +0200

    Update ChatServe.java

--- ChatServe.java
+++ ChatServe.java
@@ -1,48 +1,64 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.*;
+import java.text.ParseException;
 import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ChatServe {
+
     public static void main(String[] args) {
+        int portNumber = getPortNumber(args);
+        String handle = getHandle();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter port number:");
-        //Waits for int to be entered from command line - !not input safe
-        int portNumber = scanner.nextInt();
-        //TODO NO must be command line
+        chat(portNumber, handle);
+    }
 
-        //From https://docs.oracle.com/javase/tutorial/networking/sockets/clientServer.html
+    /*
+    From https://docs.oracle.com/javase/tutorial/networking/sockets/clientServer.htm
+    Creates socket, waits for a client to connect, begins chat
+     */
+    private static void chat(int portNumber, String handle){
         try(ServerSocket serverSocket = new ServerSocket(portNumber);
             Socket clientSocket = serverSocket.accept();
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(clientSocket.getInputStream()))){
 
-            System.out.println("The server is ready to receive messages");
+            System.out.println("Connected to client");
 
 
-            String inputLine;
-            while (true) {
-                //Check if is null if so check check again
-                inputLine = "";
-                
-                if((inputLine = in.readLine()) == null){
-                    continue;
+            handle += "> ";
+
+            Lock lock = new ReentrantLock();
+
+            ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
+            String finalHandle = handle;
+
+            executor.execute(() -> {
+                try {
+                    readMessages(in, finalHandle, lock);
                 }
-
-                //Write message to receiving client
-                System.out.println(inputLine);
-
-                /*If quit message is received terminate program
-                 */
-                if (inputLine.equals("\\quit")) {
-                    //TODO you sure to close the server?
-                    System.exit(0);
+                catch (IOException e){
+                    System.err.println("IOException");
+                    System.exit(1);
                 }
-            }
+            });
+            executor.execute(() -> {
+                try {
+                    writeMessages(out, finalHandle, lock);
+                }
+                catch (IOException e){
+                    System.err.println("IOException");
+                    System.exit(1);
+                }
+            });
+
+            //Wait forever for threads to finish
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
 
         }
         catch (IOException e){
@@ -50,7 +66,105 @@ public class ChatServe {
             //Terminates the program
             System.exit(1);
         }
+        catch (InterruptedException e){
+            System.err.println("Shutting down");
+            System.exit(1);
+        }
+
+    }
+
+    private static void readMessages(BufferedReader in, String handle, Lock lock) throws IOException{
+        String inputLine;
+
+        //Used to clear user's handle from screen when new message arrives from client
+        String clearMessage = "";
+        for(int i = 0; i<handle.length(); i++){
+            clearMessage += "\\b";
+        }
+
+        while (true) {
+            if ((inputLine = in.readLine()) != null) {
+                lock.lock();
+
+                System.out.print(clearMessage);
+
+                //If quit message is received terminate program
+                if (inputLine.equals("\\quit")) {
+                    System.out.println("Client has disconnected. Closing program...");
+                    System.exit(0);
+                }
+                else {
+                    System.out.println(inputLine);
+                    System.out.println(handle);
+                }
+
+                lock.unlock();
+            }
+        }
+    }
+
+    private static void writeMessages(PrintWriter out, String handle, Lock lock) throws IOException{
+        Scanner scanner = new Scanner(System.in);
+        String outputLine;
+
+        while(true){
+            outputLine = scanner.next();
+
+            lock.lock();
+            if (outputLine.equals("\\quit")) {
+                System.out.println("Closing program...");
+                out.write("\\quit");
+                System.exit(0);
+            }
+            else{
+                out.write(outputLine);
+                System.out.print(handle);
+            }
+            lock.unlock();
+        }
+
+    }
+
+    /*
+    Retrieves port number from first arg
+    If not there request input from terminal
+     */
+    private static int getPortNumber(String[] args){
+
+        int portNumber;
+
+        if(args == null) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter port number:");
+            portNumber = scanner.nextInt();
+        }
+        else {
+            try {
+                portNumber = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                Scanner scanner = new Scanner(System.in);
+                System.err.println("Invalid Port Number");
+                System.out.println("Please enter port number:");
+                portNumber = scanner.nextInt();
+            }
+        }
+
+        return portNumber;
+    }
+
+
+    private static String getHandle(){
+        String handle;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter username: ");
+        handle = scanner.next();
+
+        while (handle.length()>10){
+            System.out.println("Username is too long. Please enter a valid username: ");
+            handle = scanner.next();
+        }
+
+        return handle;
     }
 }
-
-
